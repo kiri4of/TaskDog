d273e4154fb7997acf77e3bc7cbfcf664aa17208
Kiri4of <kopak7890@gmail.com>
2025-01-28 14:50:26 +0000

    init

---
+++ TaskDog/TaskDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskDogApp.swift
+//  TaskDog
+//
+//  Created by Kiri4of on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
